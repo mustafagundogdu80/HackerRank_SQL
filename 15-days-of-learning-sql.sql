52456ad92982d6e493323c3979d4a05d9affc6af
Mustafa Gundogdu <103361276+mustafagundogdu80@users.noreply.github.com>
2025-02-25 01:46:20 +0100

    Create 15-days-of-learning-sql.sql
    https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem

---
+++ 15-days-of-learning-sql.sql
@@ -0,0 +1,41 @@
+-- https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem
+WITH 
+  dayly_procecess_hackers AS(
+  	SELECT submission_date, hacker_id
+  	FROM Submissions
+  	GROUP BY submission_date, hacker_id
+  )
+SELECT 
+    sb.submission_date,
+    (
+        SELECT COUNT(hacker_id)
+        FROM Hackers AS sub1_h
+        WHERE sub1_h.hacker_id IN(
+            SELECT dph.hacker_id
+            FROM (dayly_procecess_hackers) AS dph
+            WHERE dph.submission_date BETWEEN DATE '2016-03-01' AND sb.submission_date
+            GROUP BY dph.hacker_id
+            HAVING COUNT(dph.submission_date) = (sb.submission_date - DATE '2016-03-01' )+ 1
+        )
+    ) AS daily_active_hackers,
+    (
+        SELECT sub2_sb.hacker_id
+        FROM Submissions AS sub2_sb 
+        WHERE sub2_sb.submission_date = sb.submission_date
+        GROUP BY sub2_sb.hacker_id
+        ORDER BY COUNT(sub2_sb.submission_id) DESC, sub2_sb.hacker_id
+        LIMIT 1
+    ) AS max_hacker_id,
+    (
+        SELECT sub3_h.name
+        FROM Submissions AS sub3_sb, Hackers AS sub3_h 
+        WHERE sub3_sb.hacker_id = sub3_h.hacker_id AND sub3_sb.submission_date = sb.submission_date
+        GROUP BY sub3_h.hacker_id, sub3_h.name
+        ORDER BY COUNT(sub3_sb.submission_id) DESC, sub3_h.hacker_id
+        LIMIT 1
+    ) AS hacker_name
+ 
+FROM Submissions AS sb
+WHERE sb.submission_date BETWEEN DATE '2016-03-01' AND DATE '2016-03-15'
+GROUP BY sb.submission_date
+ORDER BY sb.submission_date;
